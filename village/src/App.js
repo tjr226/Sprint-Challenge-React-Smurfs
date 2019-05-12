43caefe40f6669a9bb01a64087b9a54858950078
tjr226 <tjr226@gmail.com>
2019-05-12 10:54:21 -0400

    GET request working

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
+
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -10,6 +12,13 @@ class App extends Component {
     this.state = {
       smurfs: [],
     };
+  }
+
+  componentDidMount() {
+    Axios.get('http://localhost:3333/smurfs')
+      .then(res => {
+        this.setState(() => ({ smurfs: res.data }))})
+      .catch(err => console.log(err));
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
